3a60188b11ed5d774851a08a9313b2fa355054c2
Krystian <krystianzielinski2tor@gmail.com>
2023-02-28 01:39:08 +0100

    Second section completed.

--- src/app/shared/components/featured-book/featured-book.component.ts
+++ src/app/shared/components/featured-book/featured-book.component.ts
@@ -5,4 +5,14 @@ import { Component } from '@angular/core';
   templateUrl: './featured-book.component.html',
   styleUrls: ['./featured-book.component.scss'],
 })
-export class FeaturedBookComponent {}
+export class FeaturedBookComponent {
+  featuredBookData:{h3:string, imgUrl:string, p:string, h4:string, btn:string }[] = [
+    {
+      h3: "Read Kimetsu no Yaiba manga",
+      imgUrl: "assets/img/Manga.webp",
+      p: "Demon Slayer: Kimetsu No Yaiba, Volume 20 continues as the Demon Slayer Corps' initial confrontation with the most powerful of Muzan's demons, Kokushibo, has left Tokito severely wounded and Genya cut in half. Miraculously Genya is still alive hoping that his regenerative power can heal even his fatal wound.",
+      h4: "Demon Slayer: Volume 20",
+      btn: "Order now",
+    },
+  ];
+}
